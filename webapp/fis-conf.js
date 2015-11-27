33f5ac9f865a509570db5430fd543b2edcd2c91c
dikend <dikendwd@gmail.com>
2015-11-27 19:06:25 +0800

    fis，邮箱修改

--- webapp/fis-conf.js
+++ webapp/fis-conf.js
@@ -20,21 +20,21 @@
 // });
 
 
-fis.match('*.{js,css,png}', {
+fis.match('*.{js,css}', {
   useHash: true
 });
 
-fis.match('*.js', {
-	  // fis-optimizer-uglify-js 插件进行压缩，已内置
-	  optimizer: fis.plugin('uglify-js')
-	});
-
-	fis.match('*.css', {
-	  // fis-optimizer-clean-css 插件进行压缩，已内置
-	  optimizer: fis.plugin('clean-css')
-	});
-
-	fis.match('*.png', {
-	  // fis-optimizer-png-compressor 插件进行压缩，已内置
-	  optimizer: fis.plugin('png-compressor')
-	});
+//fis.match('*.js', {
+//	  // fis-optimizer-uglify-js 插件进行压缩，已内置
+//	  optimizer: fis.plugin('uglify-js')
+//	});
+//
+//	fis.match('*.css', {
+//	  // fis-optimizer-clean-css 插件进行压缩，已内置
+//	  optimizer: fis.plugin('clean-css')
+//	});
+//
+//	fis.match('*.png', {
+//	  // fis-optimizer-png-compressor 插件进行压缩，已内置
+//	  optimizer: fis.plugin('png-compressor')
+//	});
