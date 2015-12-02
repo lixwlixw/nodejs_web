24789329a29a210d95239fce10383c76ba0b0c2e
HeHongling <hank.sunday@gmail.com>
2015-12-02 09:02:56 +0800

    add docs server

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 sed -i 's/#API_PORT#/'$API_PORT'/g' /etc/nginx/nginx.conf
 sed -i 's/#API_SERVER#/'$API_SERVER'/g' /etc/nginx/nginx.conf
 
+sed -i 's/#DOC_PORT#/'$API_PORT'/g' /etc/nginx/nginx.conf
+sed -i 's/#DOC_SERVER#/'$API_SERVER'/g' /etc/nginx/nginx.conf
+
 nginx -g "daemon off;"
