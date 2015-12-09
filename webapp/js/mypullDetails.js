b9de9fe572f23b3b0a7d580a10b3c59300f15f1d
Administrator <953005034@qq.com>
2015-12-09 19:17:43 +0800

    我的发布修改

--- webapp/js/mypullDetails.js
+++ webapp/js/mypullDetails.js
@@ -2,6 +2,36 @@
  * Created by Administrator on 2015/11/25.
  */
 $(function(){
+    function getAjax(url,fun){
+        $.ajax({
+            type: "get",
+            async: false,
+            url: url,
+            success: function(msg){
+                fun(msg);
+            }
+        });
+    }
+    function judgeLabel (labels){
+        var labeldata = {
+            'label' : labels,
+            'vvclass' : '',
+            'labelV' : ''
+        };
+        if (labeldata.label == "single") {
+            labeldata.vvclass = "api";
+            labeldata.labelV = "API";
+        }
+        if (labeldata.label == "batch") {
+            labeldata.vvclass = "period";
+            labeldata.labelV = "批量数据";
+        }
+        if (labeldata.label == "flow") {
+            labeldata.vvclass = "flot-data";
+            labeldata.labelV = "流式数据";
+        }
+        return labeldata
+    };
     //得到用户登录token;
     var account= $.cookie('token');
     function getcurrpullnum(){
@@ -22,15 +52,22 @@ $(function(){
                         '<div class="body">'+
                         '<table>';
                     for(var j=0;j<msg.data[i].pulls.length;j++){
+                        var repname = msg.data[i].pulls[j].repname;
+                        var itemname = msg.data[i].pulls[j].itemname;
+                        var judgeLabel;
+                        getAjax(ngUrl+'/repositories/'+repname+'/'+itemname,function(json){
+                            var labels = json.data.label.sys.supply_style;
+                            judgeLabel = judgeLabel(labels);
+                        })
                         var times = msg.data[i].pulls[j].pulltime
                         times = times.substr(11,8);
                         str+=
                             '<tr>'+
                             '<td class="first">'+times+'</td>'+
                             '<td>'+msg.data[i].pulls[j].tag+'</td>'+
-                            '<td>'+msg.data[i].pulls[j].repname+'</td>'+
-                            '<td>'+msg.data[i].pulls[j].itemname+'</td>'+
-                            '<td class="last">'+msg.data[i].pulls[j].tag+'</td>'+
+                            '<td>'+judgeLabel+'</td>'+
+                            '<td>'+itemname+'</td>'+
+                            '<td class="last">'+repname+'</td>'+
                             '</tr>';
                     }
                     str+= '</table></div></div>';
