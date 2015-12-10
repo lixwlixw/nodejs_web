d32470f3ca0fc8281353df30fe242e7cb3de27bc
Administrator <953005034@qq.com>
2015-12-10 11:32:27 +0800

    样例数据页面跳转修改

--- webapp/js/myItemDetails.js
+++ webapp/js/myItemDetails.js
@@ -286,9 +286,9 @@ $(function(){
     function metadatabox(){
         $('.con-main').empty();
          var str =  '<div class="metadatabox">'+
-            '<div class="metadatatop">样例 <div class="metadata-icon"><a href="myMark.html?repname='+repname+'&itemname='+itemname+'&type=sample" target="_bank"></a></div></div>'+
+            '<div class="metadatatop">样例 <div class="metadata-icon"><a href="myMark.html?repname='+repname+'&itemname='+itemname+'&type=sample" ></a></div></div>'+
             '<div class="metadata-con markdown-body">'+marked(sample)+'</div>'+
-            '<div class="metadatatop">元数据 <div class="metadata-icon"><a href="myMark.html?repname='+repname+'&itemname='+itemname+'&type=meta" target="_bank"></a></div></div>'+
+            '<div class="metadatatop">元数据 <div class="metadata-icon"><a href="myMark.html?repname='+repname+'&itemname='+itemname+'&type=meta" ></a></div></div>'+
             '<div class="metadata-con markdown-body">'+marked(meta)+'</div>'+
             '</div>';
         $('.con-main').append(str);
